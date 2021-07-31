1ae133269306af5c852fbab575c2077d558d3731
Swarga <xx26@bk.ru>
2021-07-31 09:20:03 +0300

    Добавьте файлы проекта.

---
+++ pas/pas.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<long long> pascalsTriangle(int);
+long double fact(int);
+
+int main()
+{
+    cout << "Hello World!\n";
+    int n;
+    cout << "> ";
+    cin >> n;
+    cout << endl;
+
+    pascalsTriangle(n);
+
+    return 0;
+}
+// ******************************************
+vector<long long> pascalsTriangle(int n)
+{
+    vector <long long> pasT{ n };
+    int row=0;
+    while (row <= n) {
+        for (int k = 0; k <= row; k++) {
+
+            cout << "*";
+
+
+        }
+        cout << endl;
+
+        row++;
+    }
+
+    return pasT;
+
+}
+// *******************************************
+long double fact(int f)
+{
+    if (f == 0) return 1;
+    else return f * fact(f - 1);
+}
